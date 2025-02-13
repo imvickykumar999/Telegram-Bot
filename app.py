c38e2c84d9222c57b254fa21eaefc5693077df72
Vicky Kumar <imvickykumar999@gmail.com>
2025-02-13 23:44:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import subprocess
 import threading
 
 app = Flask(__name__)
-# export TELEGRAM_BOT_TOKEN="6165663083:AAHigA2Z0IUJYeuvCoGpU5OMCwv84zrx8uo"
+# set TELEGRAM_BOT_TOKEN=6165xxxx:xxxxxxxxxxxxxxxxxxxxxxzrx8uo
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 if not BOT_TOKEN:
